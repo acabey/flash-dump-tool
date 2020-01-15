b6b84de92dcae991bf19561c90b69564e8304d6f
Andrew Cabey <me@acabey.xyz>
2020-01-15 12:34:48 -0500

    Better xersa2pem help

--- xersa2pem.py
+++ xersa2pem.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import textwrap
 import logging
 from typing import List
 from typing import TypeVar
@@ -50,18 +51,44 @@ def guess_public_key_size(data: bytes) -> XeCrypt_RSA:
 def main():
     parser = argparse.ArgumentParser(prog='XeRSA2PEM',
                                      formatter_class=argparse.RawDescriptionHelpFormatter,
-                                     description='Parse big-endian XeRSA* struct formatted binaries into PEM files')
+                                     description='Parse big-endian XeRSA* struct formatted binaries into PEM files',
+                                     epilog=textwrap.dedent('''
+                                     Example Usage:
+                                     
+                                     Convert 4096-bit private key (XeRsaPriv4096) binary file to pem:
+                                     
+                                       python3 xersa2pem.py -t private -s 4096 XeRsaPriv4096.bin XeRsaPriv4096.pem
+                                       
+                                     Convert unknown size private key binary file to pem:
+                                     
+                                       python3 xersa2pem.py -t private -s guess XeRsaPriv.bin XeRsaPriv.pem
+                                       
+                                     Convert unknown size private key binary file to pem:
+                                     
+                                       python3 xersa2pem.py -t private -s guess XeRsaPriv.bin XeRsaPriv.pem
+                                       
+                                     Convert key PEM file to appropriate binary:
+                                     
+                                       python3 xersa2pem.py -r XeRsaPriv.bin XeRsaPriv.pem
+                                       
+                                     '''))
 
     parser.add_argument('xersaobj', type=str,
                         help='Path to XeRSA* struct formatted binary file')
     parser.add_argument('pempath', type=str,
                         help='Path to PEM output file')
-    parser.add_argument('-t', '--type', type=str, help='Type of XeRSA object',
-                        choices=['public', 'private'])
-    parser.add_argument('-s', '--size', default='guess', help='Size of XeRSA key',
-                        choices=['1024', '1536', '2048', '4096', 'guess'])
-    parser.add_argument('-v', '--verbose', action='store_true', help='Set verbose-level output')
-    parser.add_argument('-d', '--debug', action='store_true', help='Set debug-level output')
+    parser.add_argument('-t', '--type', type=str, required=True,
+                        choices=['public', 'private'],
+                        help='Type of XeRSA object')
+    parser.add_argument('-s', '--size', default='guess', required=False,
+                        choices=['1024', '1536', '2048', '4096', 'guess'],
+                        help='Size of XeRSA key')
+    parser.add_argument('-r', '--reverse', action='store_true', required=False,
+                        help='Reverse process: PEM to binary file')
+    parser.add_argument('-v', '--verbose', action='store_true', required=False,
+                        help='Set verbose-level output')
+    parser.add_argument('-d', '--debug', action='store_true', required=False,
+                        help='Set debug-level output')
     args = parser.parse_args()
 
     if args.verbose:
@@ -70,6 +97,22 @@ def main():
         LOGGER.setLevel(logging.DEBUG)
 
     LOGGER.info(args)
+
+    if args.reverse:
+        try:
+            with open(args.pempath, 'rb') as pem_file:
+                data = pem_file.read()
+                LOGGER.info('Read %s (%s) bytes from %s', len(data), hex(len(data)), args.pempath)
+                rsa_obj = RSA.import_key(data)  # Passphrase not supported
+                xecrypt_obj = XeCrypt_RSA.from_rsa(rsa_obj)
+
+            with open(args.xersaobj, 'wb') as xeobj_file:
+                xeobj_file.write(bytes(xecrypt_obj))
+
+        except (FileNotFoundError, IOError, ValueError) as e:
+            LOGGER.error(str(e))
+            return
+
 
     try:
         with open(args.xersaobj, 'rb') as xeobj_file:
