f74fb7fe863f4fea4657cc977fdfd127dc529369
Andrew Cabey <me@acabey.xyz>
2020-01-15 12:36:53 -0500

    Preliminary xersa2pem support. Refactored XeCrypt signing code into PSS module

--- lib/xecrypt_rsa.py
+++ lib/xecrypt_rsa.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import struct
-from hashlib import sha1
 
 import Crypto.Cipher.ARC4 as RC4
+from Crypto.Hash import SHA1
 from Crypto.PublicKey import RSA
 
+from lib.bitwise_arithmetic import clrlwi
 from lib.xecrypt import XeCryptBnQw_SwapLeBe
+from lib.xecrypt_pss import XeCrypt_PSS_SigScheme, XeCrypt_Sig
 
 
 class XeCrypt_RSA(object):
@@ -23,6 +25,43 @@ class XeCrypt_RSA(object):
         self.cqw = int.from_bytes(xecrypt_rsa_be_data[0:4], byteorder='big')
         self.dwPubExp = int.from_bytes(xecrypt_rsa_be_data[0x04:0x08], byteorder='big')
         self.qwReserved = int.from_bytes(XeCryptBnQw_SwapLeBe(xecrypt_rsa_be_data[0x08:0x10], 0x1), byteorder='little')
+
+    @classmethod
+    def from_rsa(cls, rsa_obj: RSA):
+        raise NotImplementedError('XeCrypt_RSA.from_rsa (reverse mode) is not yet implemented')  # TODO
+
+        size_bits = rsa_obj.size_in_bits()
+        size_bytes = rsa_obj.size_in_bytes()
+
+        cqw_be = struct.pack(">L", bytes([0x00, 0x00, 0x20, 0x00]))
+        dwPubExp_be = struct.pack(">L", bytes([0x00, 0x00, 0x20, 0x00]))
+        qwReserved_be = struct.pack(">Q", bytes([0x00] * 8))
+        aqwM_be = rsa_obj.n.to_bytes(size_bytes, byteorder='big_endian', signed=False)
+
+        xeKeyPub = cqw_be + dwPubExp_be + qwReserved_be + aqwM_be
+
+        if not rsa_obj.has_private():
+            xecrypt_class = {
+                1024: XeCrypt_RSAPub_1024,
+                1536: XeCrypt_RSAPub_1536,
+                2048: XeCrypt_RSAPub_2048,
+                4096: XeCrypt_RSAPub_4096
+            }.get(size_bits)
+
+            return xecrypt_class(xeKeyPub)
+        else:
+            aqwP_be = rsa_obj.p.to_bytes(size_bytes / 2, byteorder='big_endian', signed=False)
+            aqwQ_be = rsa_obj.q.to_bytes(size_bytes / 2, byteorder='big_endian', signed=False)
+            aqwDP_be = rsa_obj.dp.to_bytes(size_bytes / 2, byteorder='big_endian', signed=False)
+            aqwDQ_be = rsa_obj.dq.to_bytes(size_bytes / 2, byteorder='big_endian', signed=False)
+            aqwCR_be = rsa_obj.u.to_bytes(size_bytes / 2, byteorder='big_endian', signed=False)
+            xecrypt_class = {
+                1024: XeCrypt_RSAPrv_1024,
+                1536: XeCrypt_RSAPrv_1536,
+                2048: XeCrypt_RSAPrv_2048,
+                4096: XeCrypt_RSAPrv_4096
+            }.get(size_bits)
+            return xecrypt_class(xeKeyPub + aqwP_be + aqwQ_be + aqwDP_be + aqwDQ_be + aqwCR_be)
 
 
 class XeCrypt_RsaPub_Abs(XeCrypt_RSA):
@@ -225,12 +264,119 @@ class XeCrypt_RSAPub_4096(XeCrypt_RsaPub_Abs):
         self.aqwM = int.from_bytes(XeCryptBnQw_SwapLeBe(data[0x10:0x10 + 0x200], 0x20), byteorder='little')
 
 
-def XeCryptBnQwBeSigCreate(hash: bytes, salt: bytes, key: XeCrypt_RsaPrv_Abs) -> bytes:
+def XeCryptBnQwBeSigFormat(hash_input: bytes, salt: bytes, key: XeCrypt_PSS_SigScheme) -> XeCrypt_Sig:
     """
-    #bool XeCryptBnQwBeSigCreate(u64* output, const u8* hash, const u8* salt, const XeRsaKey* key)
-    # export 356
-    # returns:	TRUE if successful
-    #			FALSE if error
+    Export 357
+
+    Generates PKCS #1 RSASSA-PSS signature using SHA1 for hashing and RC4 as a type of MGF
+    MGF deviates from PKCS specifcation
+
+    void XeCryptBnQwBeSigFormat(XeCrypt_PSS_Sig* output, const u8* pHash, const u8* pSalt);
+
+    :param hash_input: 20 byte digest
+    :param salt: 10 byte arbitrary data
+    :param key: implements logic using pycryptodome PSS_SigScheme object
+    :return: bytes representing XeCrypt_PSS_Sig Struct
+    """
+    return key.XeCryptBnQwBeSigFormat(hash_input, salt)
+
+
+def XeCryptBnQwBeSigCreate(hash_input: bytes, salt: bytes, key: XeCrypt_PSS_SigScheme) -> bytes:
+    """
+    Export 357
+
+    Generates PKCS #1 RSASSA-PSS signature using SHA1 for hashing and RC4 as a type of MGF
+    MGF deviates from PKCS specifcation
+
+    :param hash_input: 20 byte digest
+    :param salt: 10 byte arbitrary data
+    :param key: implements logic using pycryptodome PSS_SigScheme object
+    :return: bytes representing XeCrypt_PSS_Sig Struct
+    """
+    return key.XeCryptBnQwBeSigCreate(hash_input, salt)
+
+
+def Old_XeCryptBnQwBeSigFormat(hash_input: bytes, salt: bytes) -> bytes:
+    """
+    Export 357
+
+    Generates PKCS #1 RSASSA-PSS signature using SHA1 for hashing and RC4 as a type of MGF
+    MGF deviates from PKCS specifcation
+
+    void XeCryptBnQwBeSigFormat(XeCrypt_PSS_Sig* output, const u8* pHash, const u8* pSalt);
+
+    :param hash_input: 20 byte digest
+    :param salt: 10 byte arbitrary data
+    :return: bytes representing XeCrypt_PSS_Sig Struct
+    """
+    output = bytearray(XeCrypt_Sig.STRUCT_SIZE)
+    output[0:0xE0] = [0] * 0xE0  # Zero out first 0xE0 bytes
+
+    output[0xE0] = 0x1
+
+    output[0xE1:0xE1 + 0xA] = salt  # Copy salt into output starting at 0xE1
+
+    output[0xFF] = 0xBC
+
+    sha_ctx = SHA1.new()
+    sha_ctx.update(output[0:8])
+    sha_ctx.update(hash_input[0:0x14])
+    sha_ctx.update(salt[0:0xA])
+
+    hash_input = sha_ctx.digest()  # Re-assign, does not change value in place
+    output[0xEB: 0xEB + 0x14] = hash_input
+
+    rc4_ctx = RC4.new(hash_input)
+    encrypted = rc4_ctx.encrypt(bytes(output[0:0xEB]))
+    output[0:0xEB] = encrypted
+
+    # temp = output[0] & 0xD  # lbz r9, 0(output) | clrlwi r9, r9, 25
+    temp = clrlwi(output[0], 25)  # lbz r9, 0(output) | clrlwi r9, r9, 25
+    index11 = 0xF8
+    index10 = 0
+
+    # Skipped what I think is an alignment check? Maybe an endian check?
+    # addi r11, output, 0xF8 | cmplw cr6, output, r11 | bge cr6, end
+    # if &output & 0x00000000FFFFFFFF > (&output + index11) & 0x00000000FFFFFFFF: return
+
+    output[0] = temp.value
+
+    while index10 < index11:
+        r9 = int.from_bytes(output[index11:index11 + 8], byteorder='big',
+                            signed=False)  # ld r9, 0(r11) # r9 = (DWORD) r11[0]
+        r8 = int.from_bytes(output[index10:index10 + 8], byteorder='big',
+                            signed=False)  # ld r8, 0(r10) # r8 = (DWORD) r10[0]
+
+        output[index10:index10 + 8] = r9.to_bytes(8, byteorder='big',
+                                                  signed=False)  # std r9, 0(r10) # r10[0] = (DWORD) r9
+        index10 += 8  # addi r10, r10, 8 # r10 += 0x8
+
+        output[index11:index11 + 8] = r8.to_bytes(8, byteorder='big',
+                                                  signed=False)  # std r8, 0(r11) # r11[0] = (DWORD) r8
+        index11 -= 8  # addi r11, r11, -8  # r11 -= 0x8
+        # cmplw cr6, r10, r11
+        # blt cr6, swapping_loop
+
+    return XeCrypt_Sig.from_bytes(bytes(output))
+
+
+def Old_XeCryptBnQwBeSigCreate(hash_input: bytes, salt: bytes, key: XeCrypt_RsaPrv_Abs) -> bytes:
+    """
+    Export 356
+
+    Generates PKCS #1 RSASSA-PSS signature using SHA1 for hashing and RC4 as a type of MGF
+    MGF deviates from PKCS specifcation
+
+    bool XeCryptBnQwBeSigCreate(XeCrypt_PSS_Sig* output, const u8* pHash, const u8* pSalt, const XECRYPT_RSA* key)
+    returns: TRUE if successful
+             FALSE if error
+
+
+    :param hash_input: 20 byte digest
+    :param salt: 10 byte arbitrary data
+    :param key: implements logic using pycryptodome PSS_SigScheme object
+    :return: bytes representing XeCrypt_PSS_Sig Struct
+
     """
     if not (key.cqw == 0x00000020):
         raise ValueError('Invalid cqw')
@@ -238,27 +384,41 @@ def XeCryptBnQwBeSigCreate(hash: bytes, salt: bytes, key: XeCrypt_RsaPrv_Abs) ->
     if not (key.dwPubExp == 0x00010001 or key.dwPubExp == 0x00000011):
         raise ValueError('Invalid public exponent')
 
-    output = XeCryptBnQwBeSigFormat(hash, salt)
+    output = XeCryptBnQwBeSigFormat(hash_input, salt)
     output_bn = int.from_bytes(XeCryptBnQw_SwapLeBe(output[0:0x20], 0x4), byteorder='little')
 
-    # I don't understand this check? There is no way output could have gotten the modulus
-    if XeCryptBnQwNeCompare(output_bn, key.aqwM):
+    # Ensure large enough modulus to encrypt data
+    if XeCryptBnQwNeCompare(output_bn, key.aqwM) > 0:
         raise ValueError('Invalid sigformat output')
 
-    buffer320 = bytearray(0x20)
-    buffer320[0:0x20] = [0] * 0x20  # XeCryptBnQw_Zero
+    buffer320 = bytearray(0x20 * 8)
+    buffer320[0:0x20] = [0] * 0x20 * 8  # XeCryptBnQw_Zero
 
     r11 = 0x00000000FFFFFFFF & key.dwPubExp
     r11 -= 1
     r11 = r11 << 11 & 0xFFFFFFFF00000000  # slwi r11, r11, 11
     buffer320[0:8] = r11.to_bytes(8, byteorder='big', signed=False)
+    bn320 = r11
 
-    buffer220 = bytearray(0x20)
-    buffer220[0:0x20] = [0] * 0x20  # XeCryptBnQw_Zero
-
+    buffer220 = bytearray(0x20 * 8)
+    buffer220[0:0x20] = [0] * 0x20 * 8  # XeCryptBnQw_Zero
     r11 = 2
     buffer220[0:8] = r11.to_bytes(8, byteorder='big', signed=False)
-    XeCryptBnQwNeModExp
+    bn220 = r11
+
+    bn320 = (bn220 ^ bn320) % key.aqwM  # XeCryptBnQwNeModExp(bn_320, bn_220, bn_320, mod)
+
+
+def XeCryptBnQwBeSigVerify(sig: bytes, hash: bytes, salt: bytes, key: XeCrypt_RsaPub_Abs) -> bool:
+    """
+    Export 358
+
+    bool XeCryptBnQwBeSigVerify(XeCrypt_PSS_Sig* pSig, const u8* pbHash, const u8* pbSalt, const XECRYPT_RSA* pKey);
+
+    returns:   TRUE if successful
+               FALSE if error
+    """
+    pass
 
 
 def XeCryptBnQwNeCompare(num1: int, num2: int) -> bool:
@@ -273,113 +433,51 @@ def XeCryptBnQwNeCompare(num1: int, num2: int) -> bool:
     return num1 == num2
 
 
-def XeCryptBnQwBeSigFormat(hash: bytes, salt: bytes, size=0x100) -> bytes:
-    """
-    # export 357
-    void XeCryptBnQwBeSigFormat(u8* output, const u8* hash, const u8* salt);
-    """
-    output = bytearray(size)
-    output[0:0xE0] = [0] * 0xE0  # Zero out first 0xE0 bytes
-
-    output[0xE0] = 0x1
-
-    output[0xE1:0xE1 + 0xA] = salt  # Copy salt into output starting at 0xE1
-
-    output[0xFF] = 0xBC
-
-    sha_ctx = sha1()
-    sha_ctx.update(output[0:8])
-    sha_ctx.update(hash[0:0x14])
-    sha_ctx.update(salt[0:0xA])
-
-    hash = sha_ctx.digest()  # Re-assign, does not change value in place
-    output[0xEB: 0xEB + 0x14] = hash
-
-    rc4_ctx = RC4.new(hash)
-    encrypted = rc4_ctx.encrypt(output[0:0xEB])
-    output[0:0xEB] = encrypted
-
-    temp = output[0] & 0xD  # lbz r9, 0(output) | clrlwi r9, r9, 25
-    index11 = 0xF8
-    index10 = 0
-
-    # Skipped what I think is an alignment check? Maybe an endian check?
-    # addi r11, output, 0xF8 | cmplw cr6, output, r11 | bge cr6, end
-    # if &output & 0x00000000FFFFFFFF > (&output + index11) & 0x00000000FFFFFFFF: return
-
-    output[0] = temp
-
-    while index10 < index11:
-        r9 = int.frombytes(output[index11:index11 + 8], byteorder='big',
-                           signed=False)  # ld r9, 0(r11) # r9 = (DWORD) r11[0]
-        r8 = int.frombytes(output[index10:index10 + 8], byteorder='big',
-                           signed=False)  # ld r8, 0(r10) # r8 = (DWORD) r10[0]
-
-        output[index10:index10 + 8] = r9.to_bytes(8, byteorder='big',
-                                                  signed=False)  # std r9, 0(r10) # r10[0] = (DWORD) r9
-        index10 += 8  # addi r10, r10, 8 # r10 += 0x8
-
-        output[index11:index11 + 8] = r8.to_bytes(8, byteorder='big',
-                                                  signed=False)  # std r8, 0(r11) # r11[0] = (DWORD) r8
-        index11 -= 8  # addi r11, r11, -8  # r11 -= 0x8
-        # cmplw cr6, r10, r11
-        # blt cr6, swapping_loop
-
-    return bytes(output)
-
-
-def XeCryptBnQwBeSigVerify(sig: bytes, hash: bytes, salt: bytes, key: XeCrypt_RsaPub_Abs) -> bool:
-    """
-    # export 358
-    # returns:	TRUE if successful
-    #			FALSE if error
-    bool XeCryptBnQwBeSigVerify(u64* sig, const u8* hash, const u8* salt, const XeRsaKey* key);
-    """
-    pass
-
-
 def XeCryptBnQwNeModExp(input: int, exponent: int, modulus: int) -> int:
     """
-    # export 359
-    #
-    # args:	big number output for result of mod-exp
-    #			big number input to get mod-exp of
-    #			big number of exponent
-    #			big number of modulus
-    #			size of big number buffers (in u64 units)
-    # returns:	TRUE if successful
-    #			FALSE if error
+    Export 359
+
     bool XeCryptBnQwNeModExp(u64* bnOutput, const u64* bnInput, const u64* bnExponent, const u64* bnModulus, s32 size);
+
+    args:    big number output for result of mod-exp
+             big number input to get mod-exp of
+             big number of exponent
+             big number of modulus
+             size of big number buffers (in u64 units)
+    returns: TRUE if successful
+             FALSE if error
     """
 
 
 def XeCryptBnQwNeModExpRoot(input: int, p: int, q: int, dp: int, dq: int, cr: int) -> int:
     """
-    # export 360
-    # (basically encrypts data with parts of the rsa private key)
-    #
-    # args:	output data (encrypted data)
-    #			input data  (unencrypted data)
-    #			private key - P
-    #			private key - Q
-    #			private key - DP
-    #			private key - DQ
-    #			private key - CR (chinese remainder)
-    #			size
-    # returns:	TRUE if successful
-    #			FALSE if error
+    Export 360
+
     bool XeCryptBnQwNeModExpRoot(u64* bnOutput, const u64* bnInput, const u64* P, const u64* Q, const u64* DP, const u64* DQ, const u64* CR, s32 size);
+
+    Encrypts data with parts of the rsa private key
+
+    args:    output data (encrypted data)
+               input data  (unencrypted data)
+               private key - P
+               private key - Q
+               private key - DP
+               private key - DQ
+               private key - CR (chinese remainder)
+               size
+    returns:    TRUE if successful
+               FALSE if error
     """
     pass
 
 
 def XeCryptBnQwNeModInv(num: int) -> int:
     """
-    # export 361
-    # ??
-    #
-    # args:	number to calculate mod inverse of ?
-    # returns:	calculated value
+    Export 361
+    ??
+
+    args:    number to calculate mod inverse of ?
+    returns:    calculated value
     u64 XeCryptBnQwNeModInv(u64 num);
     """
     pass
@@ -387,22 +485,23 @@ def XeCryptBnQwNeModInv(num: int) -> int:
 
 def XeCryptBnQwNeModMul(num1: int, num2: int, mod_inv: int, modulus: int) -> int:
     """
-    # export 362
+    Export 362
     void XeCryptBnQwNeModMul(const u64* bnNum1, const u64* bnNum2, u64* bnOutput, u64 mod_inv, const u64* bnModulus, s32 bigNumSize);
     """
     pass
 
 
-def XeCryptBnQwNeRsaPrvCrypt(input: bytes, key: XeCrypt_RsaPrv_Abs) -> bytes:
+def XeCryptBnQwNeRsaPrvCrypt(message_input: bytes, key: XeCrypt_RsaPrv_Abs) -> bytes:
     """
-    # export 364
-    # ??
-    #
-    # returns:	TRUE if successful
-    #			FALSE if error
-    bool XeCryptBnQwNeRsaPrvCrypt(const u64* input, u64* output, const XeRsaKey* key);
+    Export 364
 
-    Encrypt a given Bn input in u64 blocks
+    bool XeCryptBnQwNeRsaPrvCrypt(const u64* message_input, u64* output, const XeRsaKey* key);
+
+    Encrypt a given Bn message_input with private key in u64 blocks
+
+    returns:   TRUE if successful
+               FALSE if error
+
     """
     # Unpack the key into it's components
     n = key.aqwM
@@ -410,39 +509,41 @@ def XeCryptBnQwNeRsaPrvCrypt(input: bytes, key: XeCrypt_RsaPrv_Abs) -> bytes:
     d = modinv(e, (key.aqwP - 1) * (key.aqwQ - 1))
 
     # Encrypt each qw in the plaintext a^b mod m
-    cipher_text = bytearray(len(input))
-    for i in range(int(len(input) / 8)):
+    cipher_text = bytearray(len(message_input))
+    for i in range(int(len(message_input) / 8)):
         offset = i * 8
 
-        u64 = struct.unpack(">Q", input[offset:offset + 8])[0]
+        u64 = struct.unpack(">Q", message_input[offset:offset + 8])[0]
         cipher_block = int(u64 ** d) % n
         cipher_text[offset:offset + 8] = cipher_block
 
     return bytes(cipher_text)
 
 
-def XeCryptBnQwNeRsaPubCrypt(input: bytes, key: XeCrypt_RsaPub_Abs) -> bytes:
+def XeCryptBnQwNeRsaPubCrypt(message_input: bytes, key: XeCrypt_RsaPub_Abs) -> bytes:
     """
-    # export 365
-    # crypt data with public key
-    #
-    # args:	input data to crypt
-    #			output for crypted data
-    #			public key to crypt with
-    # returns:	TRUE if successful
-    #			FALSE if error
-    bool XeCryptBnQwNeRsaPubCrypt(const u64* input, u64* output, const XeRsaKey* key);
+    Export 365
+
+    bool XeCryptBnQwNeRsaPubCrypt(const u64* message_input, u64* output, const XeRsaKey* key);
+
+    Encrypt data with public key
+
+    args:    message_input data to crypt
+                output for crypted data
+                public key to crypt with
+    returns:    TRUE if successful
+                FALSE if error
     """
     pass
 
 
 def egcd(a: int, b: int) -> int:
     """
-    Calculate the Phi or something
+    Extended Euclidean Algorithm. Returns (gcd(a,b), x, y) where ax + by = gcd(a,b)
 
-    :param a:
-    :param b:
-    :return:
+    :param a: arbitrary int
+    :param b: arbitrary int
+    :return: (gcd(a,b), x, y) where ax + by = gcd(a,b)
 
     Took from SO https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
     """
@@ -458,8 +559,8 @@ def egcd(a: int, b: int) -> int:
 def modinv(a: int, m: int) -> int:
     """
     Multiplicative modular inverse of a
-    :param a:
-    :param m:
+    :param a: arbitary int
+    :param m: arbitrary int (modulus)
     :return:
     :raises ValueError if modinv does not exist
     """
